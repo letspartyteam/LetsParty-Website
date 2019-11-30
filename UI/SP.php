de874d6aa505629c57362d146bb03352914c0bc2
Ayah alhamdan <alhamdan.ayah@gmail.com>
2019-11-30 12:10:01 +0300

    sp page added

--- UI/SP.php
+++ UI/SP.php
@@ -1,1 +1,48 @@
-service pkhk
+
+
+<!DOCTYPE html>
+<html lang="en">
+<title>Framboise and Olives</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<style>
+body {font-family: "Lato", sans-serif}
+.mySlides {display: none}
+.w3-black{
+    background-color: #f67e7d !important;
+}
+
+.w3-button:hover{
+    background-color: white !important;
+    color: #f67e7d !important;
+}
+</style>
+<body>
+
+<!-- Navbar -->
+<div class="w3-top">
+  <div class="w3-bar w3-black w3-card">
+    <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+    <a href="#" class="w3-bar-item w3-padding-large" style="text-decoration: none;"><span><img src ="images/white-logo.png" width="30" height="30"> LetsParty</span></a>
+    <a href="sp.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small"  >ORDERS</a>
+    <a href="#tour" class="w3-bar-item w3-button w3-padding-large w3-hide-small">MESSAGES</a>
+    <a href="logout.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small w3-right">Logout</a>
+    <a href="#" class="w3-bar-item w3-padding-large w3-right" style="text-decoration: none;"><span><img src ="images/SP/framboiselogo.png" width="30" height="30"> Framboise & Olive</span></a>
+  </div>
+</div>
+
+<!-- Page Content -->
+<div class="w3-content" style="max-width:2000px; margin:70px 40px">
+    
+
+    
+</div>
+
+
+
+</body>
+</html>
